0ac8be1b5788066e74e33b3263b91cdb2ab44ef8
seriouslyoncrac <o1701lane@gmail.com>
2023-12-17 05:05:09 +0000

    Local & Server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,7 @@ Rails.application.configure do # rubocop:todo Metrics/BlockLength
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
+  config.hosts.clear
 
   # Do not eager load code on boot.
   config.eager_load = false
